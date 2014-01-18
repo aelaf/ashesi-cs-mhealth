654f10411d77c934936b94d8e957333dad6add92
florence jones <floren.jones@gmail.com>
2014-01-18 11:12:45 +0000

    Health promotions branch

--- mhealth/src/com/ashesi/cs/mhealth/data/HealthPromotion.java
+++ mhealth/src/com/ashesi/cs/mhealth/data/HealthPromotion.java
@@ -1,5 +1,80 @@
 package com.ashesi.cs.mhealth.data;
 
 public class HealthPromotion {
-
+	private String date;
+	private String venue;
+	private String topic;
+	private String method;
+	private String target_audience;
+	private int number_of_audience;
+	private String remarks;
+	private String month;
+	private String latitude;
+	private String longitude;
+	private String image;
+	private int idcho;
+	private int subdistrict_id;
+	
+	public HealthPromotion(String date,String venue,String topic,String method,String target_audience,int number_of_audience,
+			String remarks, String month, String latitude, String longitude, String image, int idcho, int subdistrict_id){
+		this.date=date;
+		this.venue=venue;
+		this.topic=topic;
+		this.method=method;
+		this.target_audience=target_audience;
+		this.number_of_audience=number_of_audience;
+		this.idcho=idcho;
+		this.remarks=remarks;
+		this.month=month;
+		this.latitude=latitude;
+		this.longitude=longitude;
+		this.image=image;
+		this.subdistrict_id=subdistrict_id;
+		
+	}
+	public String getDate(){
+		return date;
+	}
+	
+	public String getVenue(){
+		return venue;
+	}
+	
+	public String getTopic(){
+		return topic;
+	}
+	
+	public String getMethod(){
+		return method;
+	}
+	
+	public String getTargetAudience(){
+		return target_audience;
+	}
+	public int getNumberAudience(){
+		return number_of_audience;
+	}
+	
+	public int getChoId(){
+		return idcho;
+	}
+	public String getRemarks(){
+		return remarks;
+	}
+	
+	public String getMonth(){
+		return month;
+	}
+	public String getLatitude(){
+		return latitude;
+	}
+	public String getLongitude(){
+		return longitude;
+	}
+	public String getImage(){
+		return image;
+	}
+	public int getSubdistrictid(){
+		return subdistrict_id;
+	}
 }
--- mhealth/src/com/ashesi/cs/mhealth/data/HealthPromotions.java
+++ mhealth/src/com/ashesi/cs/mhealth/data/HealthPromotions.java
@@ -1,5 +1,59 @@
 package com.ashesi.cs.mhealth.data;
 
-public class HealthPromotions {
 
+
+import com.ashesi.cs.mhealth.DataClass;
+import android.content.Context;
+
+
+public class HealthPromotions extends DataClass {
+	public static final String REC_DATE="date";
+	public static final String REC_NO="rec_no";
+	public static final String REC_VENUE="venue";
+	public static final String REC_TOPIC="topic";
+	public static final String REC_METHOD="method";
+	public static final String REC_TARGETAUDIENCE="target_audience";
+	public static final String REC_NUMBERAUDIENCE="number_of_audience";
+	public static final String REC_REMARKS="remarks";
+	public static final String REC_MONTH="month";
+	public static final String REC_LATITUDE="latitude";
+	public static final String REC_LONGITUDE="longitude";
+	public static final String REC_IMAGE="image";
+	public static final String REC_IDCHO="idcho";
+	public static final String SERVER_REC_NO="server_rec_no";
+	public static final String REC_SUBDISTRICTID="subdistrict_id";
+	public static final String TABLE_NAME_HEALTH_PROMOTION="health_promotion";
+	public HealthPromotions(Context context){
+		super(context);
+	}
+	
+	
+	/**
+	 * 
+	 * 
+	 * @return
+	 */
+	
+	public static String getCreateSQLString(){
+		return "create table "+ TABLE_NAME_HEALTH_PROMOTION +"( "
+				+REC_NO +" integer primary key,"
+				+REC_DATE+" text, "
+				+REC_VENUE +" text, "
+				+REC_TOPIC+" text, "
+				+REC_METHOD+ " text, "
+				+REC_TARGETAUDIENCE+ " text, "
+				+REC_NUMBERAUDIENCE+ " text,"
+				+REC_REMARKS+" text, "
+				+REC_MONTH+" text, "
+				+REC_LATITUDE+" text, "
+				+REC_LONGITUDE+" text, "
+				+REC_IMAGE+" text, "
+				+REC_IDCHO+" text, "
+				+REC_SUBDISTRICTID+" text, "
+				+SERVER_REC_NO+ " integer, "
+				+REC_STATE+" integer"
+				+" )";
+	}
+	
+	
 }
